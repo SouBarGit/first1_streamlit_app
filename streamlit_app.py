177d3a8d4ae9b52ccaf776f504f88f8ad940afac
SouBarGit <116821959+SouBarGit@users.noreply.github.com>
2023-05-21 07:45:11 -0700

    Update streamlit_app.py
    2nd try

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,10 @@ streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index)) #but this
 # display the table on the page
 streamlit.dataframe(my_fruit_list) 
 
-                     
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
 
 

0f96b34bce43d8155b396460d4a06d9c4b68f9a7
SouBarGit <116821959+SouBarGit@users.noreply.github.com>
2023-05-21 07:40:53 -0700

    Update streamlit_app.py
    fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # put some pick list
-streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index)) #but this shows numbers, we need to pick another column as index
 # display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+# lets change the index to get the fruit names
+import pandas
+my_fruit_list = pandas.read("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# should get the fruit names, then set index to fruit column
+my_fruit_list = my_fruit_list.set_index('Fruit')  
+streamlit.multiselect("Pick some fruits:" list(my_fruit_list.index)
+streamlit.datafram(my_fruit_list)                      
 
 

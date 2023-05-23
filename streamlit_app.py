02313050c9692e286332194cd3f6e05d4c35c751
SouBarGit <116821959+SouBarGit@users.noreply.github.com>
2023-05-23 11:23:25 -0700

    Update streamlit_app.py
    get info about a fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,8 @@ streamlit.header('fruityvice Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
                                    
 
 

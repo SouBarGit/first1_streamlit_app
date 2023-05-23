3b6e33da7022d6c1d276b5fde33cd2e2c3c80ec4
SouBarGit <116821959+SouBarGit@users.noreply.github.com>
2023-05-23 07:58:54 -0700

    Update streamlit_app.py
    panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
-fruityvice_normalized = python.json_normalize(fruityvice_response.json())
+fruityvice_normalized = panda.json_normalize(fruityvice_response.json())
 streamlit.datafram(fruityvice_normalized)
 
